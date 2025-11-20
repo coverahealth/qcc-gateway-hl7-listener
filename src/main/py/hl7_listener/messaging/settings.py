20723b9ed7f42a6a24a7a827f5c66368dd50c0b7
Retnaswamy <135223916+RetnaswamyCovera@users.noreply.github.com>
2025-11-20 19:49:48 +0530

    [RAP-1607]: Pydantic V2 Migrations (#32)
    Co-authored-by: babukumar-covera <babu.kumar@coverahealth.com>

--- src/main/py/hl7_listener/messaging/settings.py
+++ src/main/py/hl7_listener/messaging/settings.py
@@ -1,7 +1,6 @@
-from pydantic import (
-    AnyUrl,
-    BaseSettings
-)
+from typing import ClassVar, Optional
+from pydantic import  AnyUrl
+from pydantic_settings import  BaseSettings
 
 from hl7_listener.settings import (
     QueueType,
@@ -13,21 +12,37 @@ import hl7_listener.messaging.nats as _nats
 
 
 class CloudMessagingSettings(BaseSettings):
-    OUTBOUND_QUEUE_NAME: str
-    MSG_NAMESPACE:str
+    OUTBOUND_QUEUE_NAME: Optional[str] = None
+    MSG_NAMESPACE:Optional[str] = None
+
+    _instance: ClassVar["CloudMessagingSettings"] = None
+
+    @classmethod
+    def get_settings(cls) -> "CloudMessagingSettings":
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
 
 
 class NATSSettings(BaseSettings):
     NATS_OUTGOING_SUBJECT: str = "HL7.MESSAGES"
-    NATS_SERVER_URL: str
+    NATS_SERVER_URL: Optional[str] = None
     PILOT_MODE: bool = False
+
+    _instance: ClassVar["NATSSettings"] = None
+
+    @classmethod
+    def get_settings(cls) -> "NATSSettings":
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
 
 
 MESSAGER_CONFIG_MAP = {
-    QueueType.NATS: {"settings": NATSSettings, "messager": _nats.NATSMessager},
-    QueueType.CLOUD: {"settings": CloudMessagingSettings, "messager": _cloud_messaging.CloudMessager}
+    QueueType.NATS: {"settings": NATSSettings.get_settings(), "messager": _nats.NATSMessager},
+    QueueType.CLOUD: {"settings": CloudMessagingSettings.get_settings(), "messager": _cloud_messaging.CloudMessager}
 }
 
 
-settings = MESSAGER_CONFIG_MAP[settings_.OUTBOUND_QUEUE_TYPE]["settings"]()
+settings = MESSAGER_CONFIG_MAP[settings_.OUTBOUND_QUEUE_TYPE]["settings"]
 messager = MESSAGER_CONFIG_MAP[settings_.OUTBOUND_QUEUE_TYPE]["messager"]()
